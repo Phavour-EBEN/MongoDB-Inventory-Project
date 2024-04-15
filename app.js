e3debae54d93759895277bcae1d42c34175db887
Phavour-EBEN <ainooebenezer05@gmail.com>
2024-04-15 03:52:06 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express =  require('express');
+
+// init app and middware
+
+const app = express();
+
+app.listen(3000,() => {
+    console.log('listening on port 3000');
+});
+
+//routes
+app.get('/Car_Parts', (req, res) => {
+    res.json({mssg: 'welcome to Car_Parts'});
+});
